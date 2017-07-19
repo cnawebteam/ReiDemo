ab35b4adf6ec7ee7cc29310fbc36fdd85c3b7072
Jernej Mrvar <jernej.mrvar@paysafe.com>
2017-07-19 17:27:10 +0200

    Added views and gitignore

--- home/urls.py
+++ home/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/', views.login, name='login'),
-    url(r'^users/', views.users, name='users'),
-    url(r'^campaigns/', views.campaigns, name='campaigns'),
-    url(r'^transactions/', views.transactions, name='transactions'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^users/$', views.users, name='users'),
+    url(r'^campaigns/$', views.campaigns, name='campaigns'),
+    url(r'^transactions/$', views.transactions, name='transactions'),
+    url(r'^campaigns/(?P<campaign_id>\d+)/$', views.campaign_details, name='campaign_details'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render, render_to_response
+from django.template.context_processors import csrf
 
 
 def index(request):
@@ -16,6 +17,18 @@ def users(request):
 
 def campaigns(request):
     return render_to_response('home/campaigns.html')
+
+
+def campaign_details(request, campaign_id=None):
+    if campaign_id is None:
+        raise Http404("Campaign does not exist")
+
+    args = {}
+    args.update(csrf(request))
+
+    args['campaign_id'] = campaign_id
+
+    return render_to_response('home/campaign_details.html', args)
 
 
 def transactions(request):
