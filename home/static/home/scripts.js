23b29d384bc3f1861c4fa9c8a333127b6f6d4874
Jernej Mrvar <jernej.mrvar@paysafe.com>
2017-07-24 15:30:14 +0200

    Table formatting update

--- home/static/home/scripts.js
+++ home/static/home/scripts.js
@@ -1,12 +1,14 @@
-//$(document).ready(function() {
-//  $('[data-toggle=offcanvas]').click(function() {
-//    $('.row-offcanvas').toggleClass('active');
-//  });
-//});
-
 $(document).ready(function() {
     $('#campaign_table').DataTable();
     $('#transaction_table').DataTable();
-    $('#users_table').DataTable();
-    $('#proposal_table').DataTable();
+    $('#users_table').DataTable({
+        "columnDefs": [
+        { "width": "2.5%", "targets": [0,1] },
+        { "width": "13.5%", "targets": [2,3,4,5,6,7,8] }]
+    });
+    $('#proposal_table').DataTable({
+        "columnDefs": [
+        { "width": "2.5%", "targets": [0,1] },
+        { "width": "19%", "targets": [2,3,4,5,6] }]
+    });
 } );
